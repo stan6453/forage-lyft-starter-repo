f795a56a92efb2283214a2ca835b1842eda3a1c4
Stanley Ezechukwu <stanley6453@gmail.com>
2023-10-19 22:40:43 +0100

    added the @staticmethod annotation to the methods of the CarFactory class

--- models/car/car_factory.py
+++ models/car/car_factory.py
@@ -5,26 +5,32 @@ from .car import Car
 
 class CarFactory():
     """Abstract factory responsible for constructing various car types"""
+
+    @staticmethod
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine, battery)
 
+    @staticmethod
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine, battery)
 
+    @staticmethod
     def create_palindrome(current_date, last_service_date, warning_light_on: bool):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(current_date, last_service_date)
         return Car(engine, battery)
 
+    @staticmethod
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
         return Car(engine, battery)
 
+    @staticmethod
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
